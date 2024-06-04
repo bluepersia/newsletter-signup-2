e4aaffc7c0c7f833403c42316c864e905915dff9
Marco Zimmermann <fuzzyslash@hotmail.com>
2024-06-04 14:28:56 -0600

    Thank you

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const form = document.getElementById ('cmp__form');
 const email = document.getElementById ('cmp__email');
+const cmp = document.getElementById ('cmp');
 
 form.addEventListener ('submit', submit);
 
@@ -14,4 +15,15 @@ function submit (e)
     }
 
     email.parentElement.classList.remove ('invalid');
+
+    const html = `
+    <img src="./assets/images/icon-success.svg" alt="Success"/>
+    <h1 id="cmp__title">Thanks for subscribing!</h1>
+    <p id="cmp__body">A confirmation email has been sent to <strong>ash@loremcompany.com</strong>. Please open it and click the button inside to confirm your subscription.</p>
+    <button id="cmp__btn">Dismiss message</button>
+    `
+
+    cmp.classList.add ('thanks');
+    cmp.innerHTML = html;
+    document.getElementById ('cmp__btn').addEventListener ('click', () => document.body.removeChild (cmp));
 }
